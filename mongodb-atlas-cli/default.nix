1822777f2892146e6922b319ec8869425ea51fe3
Warren Harper <warren@harperwarren.com>
2025-10-24 14:29:36 -0400

    chore: update mongodb-atlas-cli

--- mongodb-atlas-cli/default.nix
+++ mongodb-atlas-cli/default.nix
@@ -2,15 +2,16 @@
   pkgs,
   fetchFromGitHub,
   lib,
+  nix-update-script,
 }:
 pkgs.mongodb-atlas-cli.overrideAttrs (previousAttrs: rec {
-  version = "1.48.0";
+  version = "atlascli/v1.50.0";
 
   src = fetchFromGitHub {
     owner = "mongodb";
     repo = "mongodb-atlas-cli";
     tag = "atlascli/v${version}";
-    hash = "sha256-xdsW1REvd6yIsFO9j4t7Ydb9eVt1a3I3TMBYw5eeYXI=";
+    hash = "sha256-vo2vfnGKp71S9mVGK5SVi8YqNLjedzfjJhh5uK/pb4E=";
   };
 
   # Have to set this because it's set the generated derivation.
@@ -21,5 +22,5 @@ pkgs.mongodb-atlas-cli.overrideAttrs (previousAttrs: rec {
     "-X github.com/mongodb/mongodb-atlas-cli/atlascli/internal/version.Version=v${version}"
   ];
 
-  vendorHash = "sha256-Qwk4iVFvNGyam3QNJSuwCHD0BiqPIGV7qj3HfUzJNsQ=";
+  vendorHash = "sha256-qcB8Hgp/4kVsEo32E6I4GMSBbRByPTyk3Oas2CpS4Xc=";
 })
