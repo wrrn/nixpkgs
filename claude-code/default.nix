72638cb4961696ca687a4edb50a67ad0e5c801ac
Warren Harper <warren@harperwarren.com>
2025-10-30 17:52:04 -0400

    chore: update claude-code

--- claude-code/default.nix
+++ claude-code/default.nix
@@ -1,47 +1,12 @@
 ## This is a copy of https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/by-name/cl/claude-code/package.nix so that I can keep it up to date.
 {
-  lib,
-  buildNpmPackage,
+  pkgs,
   fetchzip,
-  nodejs_20,
 }:
-
-buildNpmPackage rec {
-  pname = "claude-code";
-  version = "1.0.17";
-
-  nodejs = nodejs_20; # required for sandboxed Nix builds on Darwin
-
+pkgs.claude-code.overrideAttrs (previousAttrs: rec {
+  version = "2.0.29";
   src = fetchzip {
     url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${version}.tgz";
-    hash = "sha256-RxbsAehJ4zIt86ppmMB1MPg/XFrGWuumNdQbT+ytg8A=";
+    hash = "sha256-6YN0iSX05S+CqZ4NCDM1D+vGlLNV29NpPaVd31JN/u4=";
   };
-
-  npmDepsHash = "sha256-tOaBSeUIQdd4NU+LvcryXDuJm5kVRLGh9/4FdyQrPVs=";
-
-  postPatch = ''
-    cp ${./package-lock.json} package-lock.json
-  '';
-
-  dontNpmBuild = true;
-
-  AUTHORIZED = "1";
-
-  # `claude-code` tries to auto-update by default, this disables that functionality.
-  # https://docs.anthropic.com/en/docs/agents-and-tools/claude-code/overview#environment-variables
-  postInstall = ''
-    wrapProgram $out/bin/claude \
-      --set DISABLE_AUTOUPDATER 1
-  '';
-
-  passthru.updateScript = ./update.sh;
-
-  meta = {
-    description = "An agentic coding tool that lives in your terminal, understands your codebase, and helps you code faster";
-    homepage = "https://github.com/anthropics/claude-code";
-    downloadPage = "https://www.npmjs.com/package/@anthropic-ai/claude-code";
-    license = lib.licenses.unfree;
-    maintainers = [ lib.maintainers.malo ];
-    mainProgram = "claude";
-  };
-}
+})
